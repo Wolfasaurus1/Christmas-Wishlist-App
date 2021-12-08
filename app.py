4e1187cafb6af0eacb498a838bfd22ba255333ae
Josh <thewolfinators@gmail.com>
2021-12-08 03:16:55 -0500

    Just in case I screw something up

---
+++ app.py
@@ -0,0 +1,145 @@
+import os
+import functools
+import sqlite3
+from werkzeug.security import check_password_hash
+from werkzeug.security import generate_password_hash
+from flask import Flask, redirect, url_for, session, g, request, render_template, flash
+
+app = Flask(__name__)
+app.secret_key = os.urandom(12)
+
+
+#db stuff
+
+DATABASE = "database.db"
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+        db.row_factory = sqlite3.Row
+    return db
+
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
+
+
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
+#login stuff
+
+def login_required(view):
+    """View decorator that redirects anonymous users to the login page."""
+
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for("login"))
+
+        return view(**kwargs)
+
+    return wrapped_view
+
+
+@app.before_request
+def load_logged_in_user():
+    """If a user id is stored in the session, load the user object from
+    the database into ``g.user``."""
+    user_id = session.get("user_id")
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = (
+            get_db().execute("SELECT * FROM user WHERE id = ?", (user_id,)).fetchone()
+        )
+
+
+#routes
+
+
+@app.route("/register", methods=("GET", "POST"))
+def register():
+    """Register a new user.
+    Validates that the username is not already taken. Hashes the
+    password for security.
+    """
+
+    if request.method == "POST":
+        print("hrlloooo!!!")
+        username = request.form["username"]
+        password = request.form["password"]
+
+        print(username)
+        print(password)
+        db = get_db()
+        error = None
+        if not username:
+            error = "Username is required."
+        elif not password:
+            error = "Password is required."
+
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO user (username, password) VALUES (?, ?)",
+                    (username, generate_password_hash(password)),
+                )
+                db.commit()
+            except db.IntegrityError:
+                # The username was already taken, which caused the
+                # commit to fail. Show a validation error.
+                error = f"User {username} is already registered."
+                print(error)
+            else:
+                # Success, go to the login page.
+                return redirect(url_for("login"))
+
+        flash(error)
+
+    return render_template("register.html")
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    """Log in a registered user by adding the user id to the session."""
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        db = get_db()
+        error = None
+        user = db.execute(
+            "SELECT * FROM user WHERE username = ?", (username,)
+        ).fetchone()
+
+        print(user)
+
+        if user is None:
+            error = "Incorrect username."
+        elif not check_password_hash(user["password"], password):
+            error = "Incorrect password."
+
+        if error is None:
+            # store the user id in a new session and return to the index
+            session.clear()
+            session["user_id"] = user["id"]
+            return redirect(url_for("index"))
+
+        flash(error)
+
+    return render_template("login.html")
+
+@app.route("/")
+@login_required
+def index():
+    return "you are logged in!"
